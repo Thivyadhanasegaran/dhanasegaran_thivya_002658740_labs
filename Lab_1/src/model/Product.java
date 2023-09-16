409e52d4dd9fc445415bfde0d07b3c9d10cbc0f9
Thivya D <thivya@Thivyas-MacBook-Pro.local>
2023-09-16 13:34:01 -0400

    Final Code

---
+++ Lab_1/src/model/Product.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author thivya
+ */
+public class Product {
+    
+ 
+    private String name;
+    private String description;
+    private String availNumber;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String desc) {
+        description = desc;
+    }
+
+    public String getAvailNumber() {
+        return availNumber;
+    }
+
+    public void setAvailNumber(String availNumber) {
+        this.availNumber = availNumber;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+ 
+    
+}
